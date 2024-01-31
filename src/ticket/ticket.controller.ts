5f87f7e11745d01cad46ef215ece906517e9737e
sefogi <sebast18@gmail.com>
2024-01-31 15:28:36 +0100

    vale

--- src/ticket/ticket.controller.ts
+++ src/ticket/ticket.controller.ts
@@ -16,8 +16,8 @@ export class TicketController {
   constructor(private readonly ticketService: TicketService) {}
 
   @Post()
-  create(@Body() createTicketDto: CreateTicketDto) {
-    return this.ticketService.create(createTicketDto);
+  newTransport(@Body() createTicketDto: CreateTicketDto) {
+    return this.ticketService.newTransport(createTicketDto);
   }
 
   @Get()
@@ -25,18 +25,5 @@ export class TicketController {
     return this.ticketService.findAll();
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.ticketService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateTicketDto: UpdateTicketDto) {
-    return this.ticketService.update(+id, updateTicketDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.ticketService.remove(+id);
-  }
+  
 }
--- src/ticket/ticket.service.ts
+++ src/ticket/ticket.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, Injectable } from '@nestjs/common';
 import { CreateTicketDto } from './dto/create-ticket.dto';
 import { UpdateTicketDto } from './dto/update-ticket.dto';
 import { Ticket } from './entities/ticket.entity';
@@ -11,11 +11,33 @@ import { Provider } from 'src/provider/entities/provider.entity';
 export class TicketService {
   constructor(
     @InjectRepository(Ticket) private ticketRepository: Repository<Ticket>,
-    @InjectDataSource()private datasource:DataSource
+    @InjectDataSource()private dataSource:DataSource
     // @InjectRepository(User) private userRepository: Repository<User>,
     // @InjectRepository(Provider) private providerRepository: Repository<Provider>,
   ) {}
   async newTransport(createTicketDto:CreateTicketDto){
+    const queryRunner=this.dataSource.createQueryRunner()
+    await queryRunner.connect()
+    await queryRunner.startTransaction()
+    try{
+      const availableProvider= await queryRunner.manager.findOne(Provider, {
+        where:{id:  createTicketDto.provider.id, availability:true},
+      })
+      if(!availableProvider){
+        throw new HttpException('No disponible',404);
+      }
+      const ticket= await queryRunner.manager.save(Ticket, createTicketDto);
+
+      availableProvider.availability=false;
+      await queryRunner.manager.save(Provider,availableProvider);
+      await queryRunner.commitTransaction();
+      return ticket
+    }catch  (error) {
+      await queryRunner.rollbackTransaction();
+      throw error;
+    }finally{
+      await queryRunner.release();
+    }
     
   }
 
@@ -24,9 +46,9 @@ export class TicketService {
     return this.ticketRepository.find({ relations: ['user', 'provider'] });
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} ticket`;
-  }
+  // findOne(id: number) {
+  //   return `This action returns a #${id} ticket`;
+  // }
 
  
 }
